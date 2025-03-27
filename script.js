8221e664c73f74762982f3dba17e624d4d3eb68c
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-03-27 13:40:26 +0100

    Tog bort resterande onödig JS

--- script.js
+++
@@ -1,9 +0,0 @@
-// Simulerad blockering av tråden
-setInterval(() => {
-  let start = Date.now();
-  while (Date.now() - start < 1000) {}
-}, 3000);
-
-// Skriver ut HTML direkt i dokumentet
-document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>");
-document.write('<p>Another document.write() call</p>');
